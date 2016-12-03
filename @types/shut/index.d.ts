d1f6f46bb44244ad79e8f9e45dc363936f853077
SHUT™ Ayyash <SHUT Ayyash>
2016-12-03 08:35:13 +0200

    shut types

---
+++ @types/shut/index.d.ts
@@ -0,0 +1,12 @@
+
+// add typeRoots = ["./nodemodules/shut/@types", "./nodemodules/@types"]
+interface JQueryStatic {
+
+    Sh: any;
+    ShUi: any;
+    Res: any;
+
+}
+
+
+declare var _debug: any;
