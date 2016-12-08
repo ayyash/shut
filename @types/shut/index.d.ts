536660f3456c71c84fe5083177363ae21b9c3b30
SHUT™ Ayyash <SHUT Ayyash>
2016-12-08 15:54:25 +0200

    added types

--- @types/shut/index.d.ts
+++ @types/shut/index.d.ts
@@ -5,8 +5,23 @@ interface JQueryStatic {
     Sh: any;
     ShUi: any;
     Res: any;
+    BodyLabel(key: string, options: any, fallback?: string): any;
+}
+
+interface Array<T> {
+    remove(from, to?): T[];
 
 }
 
 
+interface String {
+    toSentenceCase(): String;
+    format(...items: string[]): String;
+    isNullOrEmpty(): Boolean;
+    toBoolean(): Boolean;
+    toPrettyPrice(): String;
+    toNormalNumber(): String;
+    toPrettyNumber(): String;
+
+}
 declare var _debug: any;
